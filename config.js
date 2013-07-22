e91a648cfae2a38c649e73fd9720fa6b3cd2c924
Anders Green <anders.green@proceranetworks.com>
2013-07-22 10:11:39 +0200

    Added timestamp of server start to secret.

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ exports.port = 4321;
 exports.upload_dir = './uploads';
 exports.static_dir = './static';
 exports.db_name = './memory.db';
-exports.secret = 'PgEL5tIA2ewL42KwtS8A2dJBSafaK76j3ALaf442aBSJbu2Kx2Bz';
+exports.secret = 'PgEL5tIA2ewL42KwtS8A2dJBSafaK76j3ALaf442aBSJbu2Kx2Bz'+(new Date().getTime());
