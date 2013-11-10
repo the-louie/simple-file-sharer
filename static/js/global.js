0f478878822124e67a9b9028cb704962b2a29f71
Anders Green <anders.green@proceranetworks.com>
2013-11-10 19:56:21 +0100

    Added button to copy to clipboard at each textbox with url

--- static/js/global.js
+++ static/js/global.js
@@ -2,7 +2,7 @@ var debug = true;
 
 
 
-;(function($, window) {
+(function($, window) {
 
 	"use strict";
 
@@ -38,7 +38,7 @@ var debug = true;
 						'<div class="file ' + i +
 						'"><div class="name">' +
 						files[j].name +
-						'</div><div class="progress"><input id="result" type="text" value="Waiting..." disabled /></div></div>'
+						'</div><div class="progress"><input style="width: 800px;" class="resulttextbox" id="result" type="text" value="Waiting..." disabled /><input style="width:65px;" class="resultcopy" id="copy" type="button" value=":(" /><input style="width:35px;display:none" class="resultcopyall" id="copy" type="button" value="" /></div></div>'
 					);
 				}
 				waiting += count;
@@ -55,10 +55,26 @@ var debug = true;
 				var dataJS = jQuery.parseJSON( data );
 				var url = String(window.location.href+'/d/'+dataJS.fileName.replace(/^\.\//,'')).replace(/([^:])\/\//,'$1/');
 				if(debug){console.log('File uploaded: ', data, url);}
-				$(".file." + current_file_id + " .progress input").val(url);
+				$(".file." + current_file_id + " .progress .resulttextbox").val(url);
 				$(".file." + current_file_id + " .name").html("<a href='"+url+"'>"+ $(".file." + current_file_id + " .name").html() +"</a>");
-				if ($(".file." + current_file_id + " .progress input")) { $(".file." + current_file_id + " .progress input").removeAttr('disabled'); }
+				if ($(".file." + current_file_id + " .progress .resulttextbox")) { $(".file." + current_file_id + " .progress input").removeAttr('disabled'); }
+				//$(".file." + current_file_id + " .progress .resultcopy").text(url)
 				$(".file." + current_file_id + " .progress").val("File upload failed!");
+				$(".file." + current_file_id + " .progress .resultcopy").val('copy')
+				$(".file." + current_file_id + " .progress .resultcopy").zclip({
+					path:'/static/js/ZeroClipboard.swf',
+					copy:function(){return url;},
+					afterCopy: function(){
+						$(".file." + current_file_id + " .progress .resultcopy").attr("disabled", "disabled");
+						$(".file." + current_file_id + " .progress .resultcopy").val('copied')
+						return true;
+					}
+				});
+				// $(".file." + current_file_id + " .progress .resultcopyall").val('all')
+				// $(".file." + current_file_id + " .progress .resultcopyall").zclip({
+				// 	path:'/static/js/ZeroClipboard.swf',
+				// 	copy:function(){return uuid;}
+				// });
 			}
 			return true;
 		}
@@ -103,7 +119,7 @@ var debug = true;
 
 				if (all_files[current_file_id].size > max_file_size) {
 					if(debug){console.log('filet too large: ',all_files[current_file_id].size);}
-					$(".file." + current_file_id + " .progress input").val('File too large ('+(all_files[current_file_id].size / (1024*1024)).toFixed(0)+' MB), max size '+(max_file_size/ (1024*1024)).toFixed(0)+' MB');
+					$(".file." + current_file_id + " .progress .resulttextbox").val('File too large ('+(all_files[current_file_id].size / (1024*1024)).toFixed(0)+' MB), max size '+(max_file_size/ (1024*1024)).toFixed(0)+' MB');
 					locked = false;
 					all_files[current_file_id] = 1;
 					current_file_id++;
@@ -112,7 +128,7 @@ var debug = true;
 				}
 				if(debug){console.log("handling file: ",all_files[current_file_id]);}
 
-				$(".file." + current_file_id + " .progress input").val('Uploading...');
+				$(".file." + current_file_id + " .progress .resulttextbox").val('Uploading...');
 
 				var current_file = all_files[current_file_id],
 					reader = new FileReader();
@@ -131,5 +147,12 @@ var debug = true;
 		dropzone.addEventListener("dragover", noopHandler, false);
 		dropzone.addEventListener("drop", drop, false);
 	});
+	// $('input.resultcopy').click(function(){
+	// 	alert('hej');
+	// })
+	// $('input.resultcopy').zclip({
+	// 	path:'js/ZeroClipboard.swf',
+	// 	copy:function(){alert($('input.resultcopy').text()); return $('input.resultcopy').text();}
+	// });
 
 }(jQuery, window));
