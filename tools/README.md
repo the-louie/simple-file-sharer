f14b23aad8b514dedc11a7c090c6e124aa6d1647
Anders Green <anders.green@proceranetworks.com>
2015-03-09 08:41:06 +0100

    Some bugfixes
    replace play with afplay and some minor stuff

--- tools/README.md
+++ tools/README.md
@@ -1,13 +1,13 @@
 These scripts are made for use in Apple Automator workflows. But can probably be reused in a multitude of ways.
 
 ## sfs_upload.py
-Upload files from commandline, primary for use with Apple workflow.
+Upload files from commandline, the result is returned as a couple of rows to stdout that osx-helper.sh uses.
 
 ## osx-helper.sh
-If sfs_upload.py is used with Apple Automator this takes the output from the script and:
-* puts the url in the paste buffer
-* plays a sound
-* shows a notification
+This script takes the output from the sfs_upload.py script and:
+	* puts the url in the paste buffer
+	* plays a sound
+	* shows a notification
 
 ## sfs_screenshot.sh
-A simple script to take a screenshot and save it to a temporary file that sfs_upload.py can upload.
+A simple script to take a screenshot and save it to a temporary file and output the filename to stdout so sfs_upload.py can be used to upload it.
--- tools/sfs_notify.sh
+++ tools/sfs_notify.sh
@@ -4,12 +4,12 @@ msg=$3
 url=$4
 
 if [ "$state" = "FAIL" ]; then
-	/bin/echo -n "Upload to filedrop failed :(" | pbcopy >/dev/null 2>&1
-	osascript -e "display notification \"Upload to filedrop failed\" with title \"Filedrop failed\" subtitle \"($msg)\"" >/dev/null 2>&1
-	/usr/local/bin/play /System/Library/Sounds/Basso.aiff >/dev/null 2>&1
+	/bin/echo -n "Upload to filedrop failed :(" | /usr/bin/pbcopy >/dev/null 2>&1
+	/usr/bin/osascript -e "display notification \"Upload to filedrop failed\" with title \"Filedrop failed\" subtitle \"($msg)\"" >/dev/null 2>&1
+	/usr/bin/afplay /System/Library/Sounds/Basso.aiff >/dev/null 2>&1
 else
-	/bin/echo -n "$url" | pbcopy >/dev/null 2>&1
-	osascript -e "display notification \"The filedrop URL are on your clipboard\" with title \"Filedrop successfull\" subtitle \"($msg)\"" >/dev/null 2>&1
-	/usr/local/bin/play /System/Library/Sounds/Purr.aiff >/dev/null 2>&1
+	/bin/echo -n "$url" | /usr/bin/pbcopy >/dev/null 2>&1
+	/usr/bin/osascript -e "display notification \"The filedrop URL are on your clipboard\" with title \"Filedrop successfull\" subtitle \"($msg)\"" >/dev/null 2>&1
+	/usr/bin/afplay /System/Library/Sounds/Purr.aiff >/dev/null 2>&1
 fi
 
